d184150d2e08b08aa912d37f2b85e70d605ae119
Venkatraamankrvr <venkatraamankr19@gmail.com>
2022-01-19 00:58:31 +0530

    project6completed

---
+++ script.js
@@ -0,0 +1,54 @@
+const resultDiv = document.querySelector(".result");
+const wordEle = document.querySelector("#word");
+const phonetics = document.querySelector(".phonetics");
+const audio = document.querySelector("audio");
+const wordMeaning = document.querySelector(".word-definition");
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const handle = async (e) => {
+  // for making the enter button to work
+    if (e.keyCode == 13) {
+        const word = e.target.value;
+        // to make a call request to the api
+        const result = await fetch(url + word);
+// to make the result as view to UI
+        resultDiv.style.display = "block";
+        const data = await result.json();
+        // print the searched word
+    alert(data[0].word);
+// if the api has the input,the result is ok
+        if (result.ok) {
+            document.querySelectorAll(".wordmeaning")[0].style.removeProperty("display");
+            document.querySelectorAll(".wordmeaning")[1].style.removeProperty("display");
+            phonetics.style.removeProperty("display");
+            audio.style.removeProperty("display");
+            wordEle.innerText = data[0].word;
+            phonetics.innerText = data[0].phonetics[0].text;
+            audio.src = data[0].phonetics[0].audio;
+            wordMeaning.innerText = data[0].meanings[0].definitions[0].definition;
+            const synonymsArray = data[0].meanings[0].definitions[0].synonyms;
+            let synonymsData = "";
+            // to print the multiple words in synonyms
+            if (synonymsArray.length) {
+                for (let i = 0; i < synonymsArray.length; i++) {
+                    synonymsData += `<p class="pills">${synonymsArray[i]}</p>`
+                }
+
+            }
+            else {
+                synonymsData = `<p class="pills">No Synonyms Available</p>`;
+            }
+            document.querySelector(".synonyms").innerHTML = synonymsData;
+        }
+        // if the input is not defined or not found in dictionary.
+         else {
+            audio.style.display = "none";
+            wordEle.innerText = data.title;
+            document.querySelectorAll(".wordmeaning")[0].style.display = "none";
+            document.querySelectorAll(".wordmeaning")[1].style.display = "none";
+            phonetics.style.display = "none";
+            wordMeaning.innerText = data.message;
+            document.querySelector(".synonyms").style.display = "none";
+        }
+    }
+}
